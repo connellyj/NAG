eb9a5733ecc838d314d821c622a016de0bf6cf39
bisilaj <bisilaj@carleton.edu>
2017-03-20 16:01:29 -0500

    implemented landscape and teleporting

--- InitialViveTest/Assets/SteamVR/Extras/SteamVR_Teleporter.cs
+++ InitialViveTest/Assets/SteamVR/Extras/SteamVR_Teleporter.cs
@@ -2,8 +2,14 @@
 using System.Collections;
 
 public class SteamVR_Teleporter : MonoBehaviour
-{
-	public enum TeleportType
+{ 
+    private Vector3 pos = Vector3.zero;
+    private bool posIsValid = false;
+    private LineRenderer lr;
+    private SteamVR_TrackedController trackedController;
+    private GameObject sphere;
+
+    public enum TeleportType
 	{
 		TeleportTypeUseTerrain,
 		TeleportTypeUseCollider,
@@ -24,13 +30,13 @@ public class SteamVR_Teleporter : MonoBehaviour
 
 	void Start()
 	{
-		var trackedController = GetComponent<SteamVR_TrackedController>();
+		trackedController = GetComponent<SteamVR_TrackedController>();
 		if (trackedController == null)
 		{
 			trackedController = gameObject.AddComponent<SteamVR_TrackedController>();
 		}
-
-		trackedController.TriggerClicked += new ClickedEventHandler(DoClick);
+        
+		trackedController.TriggerUnclicked += new ClickedEventHandler(DoUnClick);
 
 		if (teleportType == TeleportType.TeleportTypeUseTerrain)
 		{
@@ -39,9 +45,21 @@ public class SteamVR_Teleporter : MonoBehaviour
 			if (t != null)
 				t.position = new Vector3(t.position.x, Terrain.activeTerrain.SampleHeight(t.position), t.position.z);
 		}
-	}
 
-	void DoClick(object sender, ClickedEventArgs e)
+        lr = gameObject.AddComponent<LineRenderer>();
+        lr.material.color = Color.cyan;
+        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Destroy(sphere.GetComponent<Collider>());
+        sphere.GetComponent<Renderer>().material.color = Color.cyan;
+        sphere.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (trackedController.triggerPressed) DoClick();
+    }
+
+    void DoUnClick(object sender, ClickedEventArgs e)
 	{
 		if (teleportOnClick)
 		{
@@ -50,63 +68,83 @@ public class SteamVR_Teleporter : MonoBehaviour
 			if (t == null)
 				return;
 
-			// Get the current Y position of the reference space
-			float refY = t.position.y;
-
-			// Create a plane at the Y position of the Play Area
-			// Then create a Ray from the origin of the controller in the direction that the controller is pointing
-			Plane plane = new Plane(Vector3.up, -refY);
-			Ray ray = new Ray(this.transform.position, transform.forward);
-
-			// Set defaults
-			bool hasGroundTarget = false;
-			float dist = 0f;
-			if (teleportType == TeleportType.TeleportTypeUseTerrain) // If we picked to use the terrain
-			{
-				RaycastHit hitInfo;
-				TerrainCollider tc = Terrain.activeTerrain.GetComponent<TerrainCollider>();
-				hasGroundTarget = tc.Raycast(ray, out hitInfo, 1000f);
-				dist = hitInfo.distance;
-			}
-			else if (teleportType == TeleportType.TeleportTypeUseCollider) // If we picked to use the collider
-			{
-				RaycastHit hitInfo;
-				hasGroundTarget = Physics.Raycast(ray, out hitInfo);
-				dist = hitInfo.distance;
-                drawLine(transform.position, hitInfo.collider.transform.position);
-			}
-			else // If we're just staying flat on the current Y axis
-			{
-				// Intersect a ray with the plane that was created earlier
-				// and output the distance along the ray that it intersects
-				hasGroundTarget = plane.Raycast(ray, out dist);
-			}
-
-			if (hasGroundTarget)
-			{
-				// Get the current Camera (head) position on the ground relative to the world
-				Vector3 headPosOnGround = new Vector3(SteamVR_Render.Top().head.position.x, refY, SteamVR_Render.Top().head.position.z);
-
-				// We need to translate the reference space along the same vector
-				// that is between the head's position on the ground and the intersection point on the ground
-				// i.e. intersectionPoint - headPosOnGround = translateVector
-				// currentReferencePosition + translateVector = finalPosition
-				t.position = t.position + (ray.origin + (ray.direction * dist)) - headPosOnGround;
-			}
+            if(posIsValid) t.position = pos;
+            lr.enabled = false;
+            sphere.SetActive(false);
 		}
 	}
 
+    void DoClick()
+    {
+        if (teleportOnClick)
+        {
+            // First get the current Transform of the the reference space (i.e. the Play Area, e.g. CameraRig prefab)
+            var t = reference;
+            if (t == null)
+                return;
+
+            // Get the current Y position of the reference space
+            float refY = t.position.y;
+
+            // Create a plane at the Y position of the Play Area
+            // Then create a Ray from the origin of the controller in the direction that the controller is pointing
+            Plane plane = new Plane(Vector3.up, -refY);
+            Ray ray = new Ray(this.transform.position, transform.forward);
+
+            // Set defaults
+            bool hasGroundTarget = false;
+            float dist = 0f;
+            if (teleportType == TeleportType.TeleportTypeUseTerrain) // If we picked to use the terrain
+            {
+                RaycastHit hitInfo;
+                TerrainCollider tc = Terrain.activeTerrain.GetComponent<TerrainCollider>();
+                hasGroundTarget = tc.Raycast(ray, out hitInfo, 1000f);
+                dist = hitInfo.distance;
+                if (hasGroundTarget) drawLine(transform.position, hitInfo.point);
+                else
+                {
+                    lr.enabled = false;
+                    sphere.SetActive(false);
+                }
+
+            }
+            else if (teleportType == TeleportType.TeleportTypeUseCollider) // If we picked to use the collider
+            {
+                RaycastHit hitInfo;
+                hasGroundTarget = Physics.Raycast(ray, out hitInfo);
+                dist = hitInfo.distance;
+            }
+            else // If we're just staying flat on the current Y axis
+            {
+                // Intersect a ray with the plane that was created earlier
+                // and output the distance along the ray that it intersects
+                hasGroundTarget = plane.Raycast(ray, out dist);
+            }
+
+            posIsValid = hasGroundTarget;
+            Vector3 headPosOnGround = new Vector3(SteamVR_Render.Top().head.position.x, refY, SteamVR_Render.Top().head.position.z);
+            pos = t.position + (ray.origin + (ray.direction * dist)) - headPosOnGround;
+        }
+    }
+
     private void drawLine(Vector3 start, Vector3 end) {
-        GameObject myLine = new GameObject();
-        myLine.transform.position = start;
-        myLine.AddComponent<LineRenderer>();
-        LineRenderer lr = myLine.GetComponent<LineRenderer>();
-        lr.startWidth = 0.1f;
+        lr.enabled = true;
+        sphere.SetActive(true);
+        lr.startWidth = 0.05f;
         lr.endWidth = 0.1f;
         lr.SetPosition(0, start);
         lr.SetPosition(1, end);
-        lr.startColor = Color.blue;
-        lr.endColor = Color.blue;
+
+        sphere.transform.position = end;
+
+        float sizeOnScreen = 10;
+        Vector3 a = Camera.main.WorldToScreenPoint(sphere.transform.position);
+        Vector3 b = new Vector3(a.x, a.y + sizeOnScreen, a.z);
+
+        Vector3 aa = Camera.main.ScreenToWorldPoint(a);
+        Vector3 bb = Camera.main.ScreenToWorldPoint(b);
+        
+        sphere.transform.localScale = Vector3.one * (aa - bb).magnitude;
     }
 }
 
